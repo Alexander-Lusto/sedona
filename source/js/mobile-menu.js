673bcf12bd16e6d0d9382e9dbd1c410a74a18eb5
Programmer <biohazard4341@gmail.com>
2020-06-29 21:47:10 +0300

    Стилизовал планшетную версию главной страницы сайта

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,6 +2,8 @@ const navigation = document.querySelector(`.navigation`);
 const openMenuButton = document.querySelector(`.header__open-menu-btn`);
 const closeMenuButton = document.querySelector(`.navigation__close-menu-btn`);
 
+navigation.classList.remove(`navigation--no-js`);
+
 openMenuButton.addEventListener(`click`, () => {
   navigation.classList.remove(`navigation--close`);
 });
