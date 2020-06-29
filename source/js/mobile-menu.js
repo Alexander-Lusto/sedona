ab89d18c3a31249cf15fdb0e8d87e0746f6c5417
Programmer <biohazard4341@gmail.com>
2020-06-29 12:49:01 +0300

    Убрал лишние выводы в консоль

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,7 +2,6 @@ const navigation = document.querySelector(`.navigation`);
 const openMenuButton = document.querySelector(`.header__open-menu-btn`);
 const closeMenuButton = document.querySelector(`.navigation__close-menu-btn`);
 
-console.log(`hello`);
 openMenuButton.addEventListener(`click`, () => {
   navigation.classList.remove(`navigation--close`);
 });
